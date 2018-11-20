634ef37729bcc57a308bebd8b8eb98d426ab0b3e
Chris Evans <Cevans@peerlessproducts.com>
2018-11-19 18:09:43 -0600

    added quizes to page.

--- HowFarApp/HowFarApp/Views/QuizesPage.xaml.cs
+++ HowFarApp/HowFarApp/Views/QuizesPage.xaml.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using HowFar.Core.Models;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -12,9 +14,33 @@ namespace HowFarApp.Views
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class QuizesPage : ContentPage
 	{
-		public QuizesPage ()
+	    private ObservableCollection<string> _items = new ObservableCollection<string>(Enum.GetNames(typeof(QuizDifficulty)));
+	    private string _selectedItem;
+
+	    public QuizesPage ()
 		{
 			InitializeComponent ();
+		    BindingContext = this;
 		}
+
+	    public string SelectedItem
+	    {
+	        get => _selectedItem;
+	        set
+	        {
+	            _selectedItem = value;
+                OnPropertyChanged();
+	        }
+	    }
+
+	    public ObservableCollection<string> Items
+	    {
+	        get => _items;
+	        set
+	        {
+	            _items = value;
+                OnPropertyChanged();
+	        }
+	    }
 	}
 }
