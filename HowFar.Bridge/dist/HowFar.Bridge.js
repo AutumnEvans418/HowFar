cb9456cbfeb22341fe6ed84aa1ecf04147143d93
Chris Evans <chrisevans9629@gmail.com>
2020-05-06 21:23:52 -0500

    added howfar bridge

---
+++ HowFar.Bridge/dist/HowFar.Bridge.js
@@ -0,0 +1,19 @@
+/**
+ * @version 1.0.0.0
+ * @copyright Copyright ©  2020
+ * @compiler Bridge.NET 17.10.1
+ */
+Bridge.assembly("HowFar.Bridge", function ($asm, globals) {
+    "use strict";
+
+    Bridge.define("HowFar.Bridge.App", {
+        main: function Main () {
+
+
+
+
+        }
+    });
+});
+
+//# sourceMappingURL=data:application/json;base64,ewogICJ2ZXJzaW9uIjogMywKICAiZmlsZSI6ICJIb3dGYXIuQnJpZGdlLmpzIiwKICAic291cmNlUm9vdCI6ICIiLAogICJzb3VyY2VzIjogW10sCiAgIm5hbWVzIjogW10sCiAgIm1hcHBpbmdzIjogIiIsCiAgInNvdXJjZXNDb250ZW50IjogW10KfQo=
---
+++ HowFar.Bridge/dist/HowFar.Bridge.meta.js
@@ -0,0 +1,8 @@
+Bridge.assembly("HowFar.Bridge", function ($asm, globals) {
+    "use strict";
+
+
+    var $m = Bridge.setMetadata,
+        $n = ["System"];
+    $m("HowFar.Bridge.App", function () { return {"att":1048577,"a":2,"m":[{"a":2,"isSynthetic":true,"n":".ctor","t":1,"sn":"ctor"},{"a":2,"n":"Main","is":true,"t":8,"sn":"Main","rt":$n[0].Void}]}; }, $n);
+});
